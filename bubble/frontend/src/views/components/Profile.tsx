68ed2a1369e85ffedde73bb27395d4fafcf55993
Raluca <tudorr30@gmail.com>
2021-05-14 17:02:20 +0300

    Add intermediary styling to profile modal

--- bubble/frontend/src/views/components/Profile.tsx
+++ bubble/frontend/src/views/components/Profile.tsx
@@ -1,15 +1,17 @@
 import React, { FC, useEffect } from 'react';
+import { Grid } from '@material-ui/core';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import { AccountCircle, Face, Fingerprint } from '@material-ui/icons';
 
 import { getUser } from '../../utils/users';
 
 interface UserType {
-  email: string,
-  first_name: string,
-  last_name: string
+  email: string;
+  first_name: string;
+  last_name: string;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -71,9 +73,30 @@ export default function Profile() {
           <div className={classes.paper}>
             <h2 id="transition-modal-title">Profile</h2>
             <p id="transition-modal-description">
-              {user.email}
-              {user.first_name}
-              {user.last_name}
+              <Grid container spacing={3} alignItems="center">
+                <Grid item>
+                  <Face />
+                </Grid>
+                <Grid item md={true} sm={true} xs={true}>
+                  {user.email}
+                </Grid>
+              </Grid>
+              <Grid container spacing={3} alignItems="center">
+                <Grid item>
+                  <AccountCircle />
+                </Grid>
+                <Grid item md={true} sm={true} xs={true}>
+                  {user.first_name}
+                </Grid>
+              </Grid>
+              <Grid container spacing={3} alignItems="center">
+                <Grid item>
+                  <AccountCircle />
+                </Grid>
+                <Grid item md={true} sm={true} xs={true}>
+                  {user.last_name}
+                </Grid>
+              </Grid>
             </p>
           </div>
         </Fade>
