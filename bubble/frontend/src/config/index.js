b3042bc61b1239a4891ecd9d94ca5228c6b8e6ed
Stefan Radu <stefan270101@gmail.com>
2021-05-14 13:09:14 +0300

    rem line in gitignore and keys from config

--- bubble/frontend/src/config/index.js
+++ bubble/frontend/src/config/index.js
@@ -3,12 +3,3 @@ exports.__esModule = true;
 exports.FIREBASE_CONFIG = exports.BACKEND_URL = exports.BASE_URL = void 0;
 exports.BASE_URL = 'http://localhost:8000';
 exports.BACKEND_URL = 'http://localhost:8000/api/v1';
-// TODO nu ma bateti le bag in env alta data ^^
-exports.FIREBASE_CONFIG = {
-    apiKey: "AIzaSyA7JsN64P52pezY9Q-LAVTDTUUJkoFDCm8",
-    authDomain: "bubble-9c539.firebaseapp.com",
-    projectId: "bubble-9c539",
-    storageBucket: "bubble-9c539.appspot.com",
-    messagingSenderId: "414287814218",
-    appId: "1:414287814218:web:80c29bb959c6a3b8a590b3"
-};
