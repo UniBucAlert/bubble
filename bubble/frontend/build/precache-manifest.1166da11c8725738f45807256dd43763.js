96877bd12491513a0ead591a61d469fa7caa6b5a
PredaMihaiDragos <predamihaidragos@gmail.com>
2021-06-10 22:41:19 +0300

    Add production build

---
+++ bubble/frontend/build/precache-manifest.1166da11c8725738f45807256dd43763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8e45c24eab130cb25388ae0f00a0fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0e28becedb7b1c862a6",
+    "url": "/static/css/main.d9f46d00.chunk.css"
+  },
+  {
+    "revision": "32e72dc55b3ebf4b3fff",
+    "url": "/static/js/2.aa9596c3.chunk.js"
+  },
+  {
+    "revision": "536099d760d182e1a9f4cc114dfb566e",
+    "url": "/static/js/2.aa9596c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e28becedb7b1c862a6",
+    "url": "/static/js/main.c7bcbc09.chunk.js"
+  },
+  {
+    "revision": "934c498a4ec70f8f5d36",
+    "url": "/static/js/runtime-main.e2ec6b1c.js"
+  }
+]);
